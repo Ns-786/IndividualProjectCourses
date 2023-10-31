073bfe7b37a9ea1b41ecd5abc84550af5b0345cd
nsbhalli <2011615@studentmail.ucol.ac.nz>
2023-11-01 10:44:32 +1300

    Create new project

--- Learning MVC/ExploreCailfornia/ExploreCailfornia/Startup.cs
+++ Learning MVC/ExploreCailfornia/ExploreCailfornia/Startup.cs
@@ -27,7 +27,7 @@ namespace ExploreCailfornia
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello ASP NET Core!");
             });
         }
     }
