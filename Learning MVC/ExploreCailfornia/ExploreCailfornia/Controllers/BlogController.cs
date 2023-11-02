bdb395484d5c3c1f9a18efbb1219b74271e20dc8
nsbhalli <2011615@studentmail.ucol.ac.nz>
2023-11-03 10:51:24 +1300

    pass parameters to controller actions

---
+++ Learning MVC/ExploreCailfornia/ExploreCailfornia/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCailfornia.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Blog posts" };
+        }
+        public IActionResult Post(int id = -1)
+
+        {
+           
+                
+            return new ContentResult { Content = id.ToString() };
+        }
+    }
+}
