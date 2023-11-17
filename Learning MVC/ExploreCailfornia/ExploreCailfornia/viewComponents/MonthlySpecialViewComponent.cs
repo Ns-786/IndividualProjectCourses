dba2816a63224f5facc0bd5c5c59fdf22d68a56f
nsbhalli <2011615@studentmail.ucol.ac.nz>
2023-11-17 20:05:05 +1300

    Create reuseable components with view components

---
+++ Learning MVC/ExploreCailfornia/ExploreCailfornia/viewComponents/MonthlySpecialViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCailfornia.viewComponents
+{   
+    [ViewComponent]
+    public class MonthlySpecialViewComponent: ViewComponent
+    {
+        public string Invoke()
+        {
+            return "TODO: Show monthly special";
+        }
+    }
+}
