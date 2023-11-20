b7ab17cb447db47a91696e9ac857819f294e0309
nsbhalli <2011615@studentmail.ucol.ac.nz>
2023-11-20 14:49:46 +1300

    Unit testingHttpGet API endpoint

--- Unit Testing/Library.API/LibraryAPI.Test/BookControllerTest.cs
+++ Unit Testing/Library.API/LibraryAPI.Test/BookControllerTest.cs
@@ -1,5 +1,7 @@
 using Library.API.Controllers;
+using Library.API.Data.Models;
 using Library.API.Data.Services;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace LibraryAPI.Test
@@ -13,6 +15,22 @@ namespace LibraryAPI.Test
         {
             _service = new BookService();
             _controller = new BooksController(_service);
+        }
+
+        [Fact]
+        public void GetAllTest()
+        {
+            //arrange
+            //act
+            var result = _controller.Get();
+            //assert
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var list = result.Result as OkObjectResult;
+            Assert.IsType<List<Book>>(list.Value);
+
+            var listBooks = list.Value as List<Book>;
+            Assert.Equal(5, listBooks.Count);
         }
         
     }
